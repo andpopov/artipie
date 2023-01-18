18d507e35ff1241163d417bd0d9a9482adcf468d
Andrey Popov <andpopov@gmail.com>
2023-01-18 13:19:23 +0300

    test

---
+++ src/test/java/com/artipie/auth/AuthFromKeycloakTest.java
@@ -0,0 +1,45 @@
+package com.artipie.auth;
+
+import com.artipie.asto.test.TestResource;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link AuthFromKeycloak}
+ */
+public class AuthFromKeycloakTest {
+    @Test
+    void docker() throws IOException {
+        final String resources = "auth/keycloak-docker-initializer";
+        final Set<Path> jars = paths(
+            new TestResource(String.format("%s/lib", resources)).asPath(), ".jar"
+        );
+        final Set<Path> sources = paths(
+            new TestResource(String.format("%s", resources)).asPath(), ".java"
+        );
+        System.out.println(sources);
+    }
+
+    private static Set<Path> paths(final Path dir, final String ext) throws IOException {
+        Set<Path> files = new HashSet<>();
+        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                if (!Files.isDirectory(file)) {
+                    if (ext == null || file.toString().endsWith(ext)) {
+                        files.add(file);
+                    }
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
+    }
+}

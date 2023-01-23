cbf7ea621672c2f0bb13714498f03a5de89fba9e
Andrey Popov <andpopov@gmail.com>
2023-01-23 18:01:15 +0300

    v1.3

--- src/test/java/com/artipie/auth/AuthFromKeycloakTest.java
+++ src/test/java/com/artipie/auth/AuthFromKeycloakTest.java
@@ -1,3 +1,7 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/artipie/LICENSE.txt
+ */
 package com.artipie.auth;
 
 import com.amihaiemil.eoyaml.Yaml;
--- src/test/java/com/artipie/tools/CodeBlob.java
+++ src/test/java/com/artipie/tools/CodeBlob.java
@@ -1,20 +1,27 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/artipie/LICENSE.txt
+ */
 package com.artipie.tools;
 
 import java.util.Objects;
 
 /**
  * Class stores classname and it's compiled byte code.
+ * @since 0.28.0
  */
-public class CodeBlob {
+public final class CodeBlob {
     /**
      * Class name of class.
      * It is used by class loader as classname.
      */
+    @SuppressWarnings("PMD.AvoidFieldNameMatchingMethodName")
     private final String classname;
 
     /**
      * Byte code of class.
      */
+    @SuppressWarnings("PMD.AvoidFieldNameMatchingMethodName")
     private final byte[] blob;
 
     /**
@@ -22,6 +29,7 @@ public class CodeBlob {
      * @param classname Class name of class.
      * @param bytes Byte code of class
      */
+    @SuppressWarnings("PMD.ArrayIsStoredDirectly")
     public CodeBlob(final String classname, final byte[] bytes) {
         this.classname = classname;
         this.blob = bytes;
@@ -32,27 +40,33 @@ public class CodeBlob {
      * @return Class name.
      */
     public String classname() {
-        return classname;
+        return this.classname;
     }
 
     /**
      * Byte code of class.
      * @return Byte code.
      */
+    @SuppressWarnings("PMD.MethodReturnsInternalArray")
     public byte[] blob() {
-        return blob;
+        return this.blob;
     }
 
+    @SuppressWarnings("PMD.OnlyOneReturn")
     @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final CodeBlob codeBlob = (CodeBlob) o;
-        return Objects.equals(classname, codeBlob.classname);
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final CodeBlob other = (CodeBlob) obj;
+        return Objects.equals(this.classname, other.classname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(classname);
+        return Objects.hash(this.classname);
     }
 }

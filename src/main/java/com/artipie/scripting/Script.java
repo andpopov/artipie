ed2d98d9a3ceef73072c2055f8170e831e929737
Andrey Popov <andpopov@gmail.com>
2023-04-07 14:42:34 +0300

    v1.0

--- src/main/java/com/artipie/scripting/Script.java
+++ src/main/java/com/artipie/scripting/Script.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Objects;
 import javax.script.Compilable;
 import javax.script.CompiledScript;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -26,23 +27,46 @@ public class Script {
         return this;
     }
 
-    public Object execute(final Map<String, Object> vars) throws ScriptException {
+    public Object call(final Map<String, Object> vars) throws ScriptException {
         Objects.requireNonNull(this.script, "Script is null");
+        final SimpleBindings bindings = new SimpleBindings(vars);
+        this.engine.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
         final Object res;
         if (this.engine instanceof Compilable && this.compiledScript != null) {
-            res = this.compiledScript.eval(new SimpleBindings(vars));
+            res = this.compiledScript.eval(bindings);
         } else {
-            res = this.engine.eval(this.script, new SimpleBindings(vars));
+            res = this.engine.eval(this.script);
         }
         return res;
     }
 
-    public Object execute(final String script, final Map<String, Object> variables)
+    public Object call() throws ScriptException {
+        Objects.requireNonNull(this.script, "Script is null");
+        final SimpleBindings bindings = new SimpleBindings();
+        this.engine.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+        final Object res;
+        if (this.engine instanceof Compilable && this.compiledScript != null) {
+            res = this.compiledScript.eval(bindings);
+        } else {
+            res = this.engine.eval(this.script);
+        }
+        return res;
+    }
+
+    public Object call(final String script, final Map<String, Object> variables)
         throws ScriptException {
-        return this.engine.eval(script, new SimpleBindings(variables));
+        this.engine.setBindings(new SimpleBindings(variables), ScriptContext.ENGINE_SCOPE);
+        return this.engine.eval(script);
+    }
+
+    public Object call(final String script)
+        throws ScriptException {
+        this.engine.setBindings(new SimpleBindings(), ScriptContext.ENGINE_SCOPE);
+        return this.engine.eval(script);
     }
 
     public Object variable(final String name) {
-        return this.engine.get(name);
+        return this.engine.getBindings(ScriptContext.ENGINE_SCOPE).get(name);
+//        return this.engine.get(name);
     }
 }
--- src/main/java/com/artipie/scripting/ScriptMain.java
+++ src/main/java/com/artipie/scripting/ScriptMain.java
@@ -1,15 +1,31 @@
 package com.artipie.scripting;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import java.util.HashMap;
+import java.util.Map;
 import javax.script.ScriptException;
 
 public class ScriptMain {
     public static void main(String[] args) throws ScriptException {
-        final ScriptEngineManager factory = new ScriptEngineManager();
-        final ScriptEngine engine = factory.getEngineByName("ruby");
-        engine.eval("puts 'Hello!'");
-        System.out.println();
+        GroovyScript script = new GroovyScript();
+        script
+            .compile("println(1)")
+            .call();
 
+        script.call("println(2)");
+
+        script
+            .compile("println(a)")
+            .call(Map.of("a", 3));
+
+        script.call("println(a)", Map.of("a", 4));
+
+        System.out.println(
+            script.call("return a*2", Map.of("a", 5))
+        );
+
+        final Map<String, Object> variables = new HashMap<>();
+        variables.put("a", 6);
+        script.call("a = a * 2", variables);
+        System.out.println(script.variable("a"));
     }
 }

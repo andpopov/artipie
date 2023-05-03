193293ec96074df9160e2a01204c930cc4166a81
Andrey Popov <andpopov@gmail.com>
2023-05-03 12:36:56 +0300

    v1.0

--- src/test/java/com/artipie/scripting/MvelScriptTest.java
+++ src/test/java/com/artipie/scripting/MvelScriptTest.java
@@ -7,6 +7,8 @@ package com.artipie.scripting;
 import java.util.HashMap;
 import java.util.Map;
 import javax.script.ScriptException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -20,13 +22,15 @@ public class MvelScriptTest {
     public void standardScript() throws ScriptException {
         MvelScript.newScript("System.out.println(1)").call();
         MvelScript.newScript("System.out.println(a)").call(Map.of("a", 2));
-        System.out.println(
-            MvelScript.newScript("a * 2").call(Map.of("a", 3)).value()
+        MatcherAssert.assertThat(
+            MvelScript.newScript("a * 2").call(Map.of("a", 3)).value(),
+            new IsEqual<>(6)
         );
         final Map<String, Object> variables = new HashMap<>();
         variables.put("a", 4);
-        System.out.println(
-            MvelScript.newScript("a = a * 3").call(variables).variable("a")
+        MatcherAssert.assertThat(
+            MvelScript.newScript("a = a * 3").call(variables).variable("a"),
+            new IsEqual<>(12)
         );
     }
 
@@ -34,13 +38,15 @@ public class MvelScriptTest {
     public void precompiledScript() throws ScriptException {
         MvelScript.newCompiledScript("System.out.println(1)").call();
         MvelScript.newCompiledScript("System.out.println(a)").call(Map.of("a", 2));
-        System.out.println(
-            MvelScript.newCompiledScript("a * 2").call(Map.of("a", 3)).value()
+        MatcherAssert.assertThat(
+            MvelScript.newCompiledScript("a * 2").call(Map.of("a", 3)).value(),
+            new IsEqual<>(6)
         );
         final Map<String, Object> variables = new HashMap<>();
         variables.put("a", 4);
-        System.out.println(
-            MvelScript.newCompiledScript("a = a * 3").call(variables).variable("a")
+        MatcherAssert.assertThat(
+            MvelScript.newCompiledScript("a = a * 3").call(variables).variable("a"),
+            new IsEqual<>(12)
         );
     }
 }

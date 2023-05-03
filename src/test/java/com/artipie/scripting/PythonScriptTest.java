d5665e3100c9803b4d44f4062d7dc4d7d05e4743
Andrey Popov <andpopov@gmail.com>
2023-05-03 12:40:30 +0300

    v1.0

--- src/test/java/com/artipie/scripting/PythonScriptTest.java
+++ src/test/java/com/artipie/scripting/PythonScriptTest.java
@@ -7,6 +7,8 @@ package com.artipie.scripting;
 import java.util.HashMap;
 import java.util.Map;
 import javax.script.ScriptException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -23,12 +25,14 @@ public class PythonScriptTest {
         variables.put("a", 2);
         PythonScript.newScript("print(a)").call(variables);
         variables.put("a", 3);
-        System.out.println(
-            PythonScript.newScript("a * 2").call(variables).value()
+        MatcherAssert.assertThat(
+            PythonScript.newScript("a * 2").call(variables).value(),
+            new IsEqual<>(6)
         );
         variables.put("a", 4);
-        System.out.println(
-            PythonScript.newScript("a = a * 3").call(variables).variable("a")
+        MatcherAssert.assertThat(
+            PythonScript.newScript("a = a * 3").call(variables).variable("a"),
+            new IsEqual<>(12)
         );
     }
 
@@ -39,12 +43,14 @@ public class PythonScriptTest {
         variables.put("a", 2);
         PythonScript.newCompiledScript("print(a)").call(variables);
         variables.put("a", 3);
-        System.out.println(
-            PythonScript.newCompiledScript("a * 2").call(variables).value()
+        MatcherAssert.assertThat(
+            PythonScript.newCompiledScript("a * 2").call(variables).value(),
+            new IsEqual<>(6)
         );
         variables.put("a", 4);
-        System.out.println(
-            PythonScript.newCompiledScript("a = a * 3").call(variables).variable("a")
+        MatcherAssert.assertThat(
+            PythonScript.newCompiledScript("a = a * 3").call(variables).variable("a"),
+            new IsEqual<>(12)
         );
     }
 }
